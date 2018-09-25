88be0e0c75a3422eeebb371a05141d3054f20f1a
jsaka1259 <17916915+jsaka1259@users.noreply.github.com>
2018-09-25 10:14:48 +0900

    Mod Func Name

--- st7032i.c
+++ st7032i.c
@@ -1,9 +1,11 @@
 #include <common.h>
 
-#define LCD_I2C_ADDR    0x7C    // LCD I2C Address
-#define CONTRAST        0x18    // CONTRAST
+#define ST7032I_I2C_ADDR 0x7C   // ST7032I I2C Address
+#define ST7032I_CONTRAST 0x18   // for 5V
+//#define ST7032I_CONTRAST 0x30   // for 3.3V
 
-void lcd_i2c_master_write(uint8_t addr, uint8_t cont, uint8_t data)
+
+void st7032i_i2c_master_write(uint8_t addr, uint8_t cont, uint8_t data)
 {
     SEN = 1;
     while(SEN);
@@ -15,9 +17,9 @@ void lcd_i2c_master_write(uint8_t addr, uint8_t cont, uint8_t data)
     while(PEN);
 }
 
-void lcd_cmd(uint8_t cmd)
+void st7032i_cmd(uint8_t cmd)
 {
-    lcd_i2c_master_write(LCD_I2C_ADDR, 0x00, cmd);
+    st7032i_i2c_master_write(ST7032I_I2C_ADDR, 0x00, cmd);
     
     /* Clear or Home */
     if(cmd & 0xFC)
@@ -30,38 +32,38 @@ void lcd_cmd(uint8_t cmd)
     }
 }
 
-void lcd_init(void)
+void st7032i_init(void)
 {
     __delay_ms(100);
-    lcd_cmd(0x38);                      // 8bit 2line Noraml mode
-    lcd_cmd(0x39);                      // 8bit 2line Extend mode
-    lcd_cmd(0x14);                      // OSC 183Hz BIAS 1/5
-    lcd_cmd(0x70 + (CONTRAST & 0x0F));  // CONTRAST
-    lcd_cmd(0x50 + (CONTRAST >> 4));    // CONTRAST
-    lcd_cmd(0x6A);                      // Follower for 5V
-    //lcd_cmd(0x6B);                     // Follower for 3.3V
+    st7032i_cmd(0x38);                      // 8bit 2line Noraml mode
+    st7032i_cmd(0x39);                      // 8bit 2line Extend mode
+    st7032i_cmd(0x14);                      // OSC 183Hz BIAS 1/5
+    st7032i_cmd(0x70 + (CONTRAST & 0x0F));  // CONTRAST
+    st7032i_cmd(0x50 + (CONTRAST >> 4));    // CONTRAST
+    st7032i_cmd(0x6A);                      // Follower for 5V
+    //st7032i_cmd(0x6B);                      // Follower for 3.3V
     __delay_ms(100);
     __delay_ms(100);
-    lcd_cmd(0x38);                      // Set Normal mode
-    lcd_cmd(0x0C);                      // Display On
-    lcd_cmd(0x01);                      // Clear Display
+    st7032i_cmd(0x38);                      // Set Normal mode
+    st7032i_cmd(0x0C);                      // Display On
+    st7032i_cmd(0x01);                      // Clear Display
 }
 
-void lcd_clear(void)
+void st7032i_clear(void)
 {
-    lcd_cmd(0x01);                      // Clear Display
+    st7032i_cmd(0x01);                      // Clear Display
 }
 
-void lcd_putc(uint8_t data)
+void st7032i_putc(uint8_t data)
 {    
-    lcd_i2c_master_write(LCD_I2C_ADDR, 0x40, data);
+    st7032i_i2c_master_write(ST7032I_I2C_ADDR, 0x40, data);
     __delay_us(50);
 }
 
-void lcd_puts(const uint8_t* buf)
+void st7032i_puts(const uint8_t* buf)
 {
     while(*buf)
     {
-        lcd_putc(*buf++);
+        st7032i_putc(*buf++);
     }
 }
--- st7032i.h
+++ st7032i.h
@@ -1,11 +1,11 @@
-#ifndef LCD_H
-#define LCD_H
+#ifndef ST7032I_H
+#define ST7032I_H
 
-extern void lcd_cmd(uint8_t);
-extern void lcd_init(void);
-extern void lcd_clear(void);
-extern void lcd_putc(uint8_t data);
-extern void lcd_puts(const uint8_t* buf);
+extern void st7032i_cmd(uint8_t);
+extern void st7032i_init(void);
+extern void st7032i_clear(void);
+extern void st7032i_putc(uint8_t data);
+extern void st7032i_puts(const uint8_t* buf);
 
-#endif /* LCD_H */
+#endif /* ST7032I_H */
 
